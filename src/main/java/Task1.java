a9f0215f7606c0a92671f9ee9a4226513d9d9cd5
Grigory <grigorydavydenko@yandex.ru>
2022-09-23 23:42:00 +0700

    solved

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -4,13 +4,20 @@ public class Task1
     //0.5 балла - если посчитаете в цикле
     //1 балл - если посчитаете рекурсией
     public static int fact(int n){
-        return n;
+        if(n == 2)
+            return n;
+        return n * fact(n - 1);
+
     }
 
     //вывести таблицу умножения на экран - 1 балл
     //подсказка - использовать двойной for
     public static void table(){
-        //Ваше решение здесь
+        for(int i=1; i < 10; i++) {
+            for(int j=1; j < 10; j++)
+                System.out.print(i * j + "\t");
+            System.out.println();
+        }
     }
 
     //посчитать сумму цифр числа
@@ -18,8 +25,12 @@ public class Task1
     //для любого числа - 1 балл
     //подсказка - в случае для любого числа используйте while
     public static int sum(int n){
-        //Ваше решение здесь
-        return n;
+        int ans=0;
+        while (n > 0) {
+            ans += n % 10;
+            n /= 10;
+        }
+        return ans;
     }
 
     //определить, является ли год високосным
@@ -30,8 +41,9 @@ public class Task1
     //Годы 2100, 2200 и 2300 - не високосные.
     //за правильный ответ - 0.5 балла
     public static boolean isLeapYear(int year) {
-        //Ваше решение здесь
-        return true;
+        if((year % 4 == 0) & !((year % 100 == 0) & (year % 400 != 0)))
+            return true;
+        return false;
     }
 
     //здесь вам нужно будет использовать результат прошлой задачи
@@ -39,25 +51,44 @@ public class Task1
     //правильный ответ - 0.5 балла
     public static int daysInYear(int year) {
         if (isLeapYear(year)){
-            //
+            return 366;
         } else {
-            //
+            return 365;
         }
-        return 0;
     }
 
     //определить номер дня недели по строке
     //например: Понедельник - 1
     //правильный ответ - 1 балл
     public static int dayOfTheWeek(String n){
-        //Ваше решение здесь
-        return 0;
+        switch (n.toLowerCase()) {
+            case "понедельник":
+                return 1;
+            case "вторник":
+                return 2;
+            case "среда":
+                return 3;
+            case "четверг":
+                return 4;
+            case "пятница":
+                return 5;
+            case "суббота":
+                return 6;
+            case "воскресенье":
+                return 7;
+            default:
+                return 0;
+        }
     }
 
     //вывести массив на экран в виде: [1, 5, 3, 7, 10, 2, 5]
     //правильное решение - 0.5 балла
     public static void printArray(int[] array){
-        //Ваше решение здесь
+        int l=array.length;
+        System.out.print("[");
+        for(int i=0; i < l - 1; i++)
+            System.out.print(array[i] + ", ");
+        System.out.println(array[l - 1] + "]");
     }
 
     //вывести двойной массив на экран в виде:
@@ -66,7 +97,14 @@ public class Task1
     // ...
     //правильное решение - 0.5 балла
     public static void printArray(int[][] array){
-        //Ваше решение здесь
+        int l=array.length;
+        for(int i=0; i < l; i++) {
+            System.out.print("[");
+            int li=array[i].length;
+            for(int j=0; j < li - 1; j++)
+                System.out.print(array[i][j] + ", ");
+            System.out.println(array[i][li - 1] + "]");
+        }
     }
 
     //отсортировать одномерный массив в порядке возрастания
@@ -74,7 +112,14 @@ public class Task1
     //метод пузырька (один из самых простых для понимания)
     //правильный ответ - 1 балл
     public static int[] sort(int[] array){
-        //Ваше решение здесь
+        int l=array.length;
+        for(int i=0; i < l ; i++)
+            for(int j=0; j < l - i - 1; j++)
+                if(array[j] > array[j + 1]) {
+                    int k = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = k;
+                }
         return array;
     }
 
